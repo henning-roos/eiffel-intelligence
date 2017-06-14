4fe0b2ec0902c82377f5367c2709e491b832c3dc
Suryakumari <sk.surya@tcs.com>
2017-06-14 20:29:17 +0530

    Implemented MongoDb querying part

--- src/main/java/com/ericsson/ei/mergeHandler/MergeHandler.java
+++ src/main/java/com/ericsson/ei/mergeHandler/MergeHandler.java
@@ -2,40 +2,84 @@ package com.ericsson.ei.mergeHandler;
 
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
 import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
 import com.mongodb.util.JSON;
 
 public class MergeHandler {
-    
+
+    MongoClient mongoClient;
+
+    //Establishing the connection to mongodb and creating a collection
+    public void createConnection(String host, int port){
+        mongoClient = new MongoClient(host , port);
+        System.out.println("connection created");
+    }
+
+    //Insert data into collection
+    public void insertDocument(String dataBaseName, String collectionName, String input){
+        DB db = mongoClient.getDB(dataBaseName);
+        DBCollection table = db.getCollection(collectionName);
+        DBObject dbObjectInput = (DBObject)JSON.parse(input);
+        table.insert(dbObjectInput);
+    }
+
+    //Retrieve entire data from  collection
+    public void getDocuments(String dataBaseName, String collectionName){
+        DB db = mongoClient.getDB(dataBaseName);
+        DBCollection table = db.getCollection(collectionName);
+        DBCursor cursor = table.find();
+        int i = 1;
+        while (cursor.hasNext()) { 
+           System.out.println("Inserted Document: "+i); 
+           System.out.println(cursor.next()); 
+           i++;
+        }
+    }
+
+    //Retrieve data from the collection based on condition
+    public void getDocumentOnCondition(String dataBaseName, String collectionName,String condition){
+        DB db = mongoClient.getDB(dataBaseName);
+        DBCollection table = db.getCollection(collectionName);
+        DBObject dbObjectCondition = (DBObject)JSON.parse(condition);
+        DBCursor conditionalCursor = table.find(dbObjectCondition);
+        while(conditionalCursor.hasNext()) {
+            System.out.println(conditionalCursor.next());
+        }
+    }
+
+    //update the document in collection
+    public void updateDocument(String dataBaseName, String collectionName, String input, String updateInput ){
+        DB db = mongoClient.getDB(dataBaseName);
+        DBCollection table = db.getCollection(collectionName);
+        DBObject dbObjectInput = (DBObject)JSON.parse(input);
+        DBObject dbObjectUpdateInput = (DBObject)JSON.parse(updateInput);
+        table.update(dbObjectInput , dbObjectUpdateInput);
+    }
+
+    //drop the document in collection
+    public void dropDocument(String dataBaseName, String collectionName,String condition){
+        DB db = mongoClient.getDB(dataBaseName);
+        DBCollection table = db.getCollection(collectionName);
+        DBObject dbObjectCondition = (DBObject)JSON.parse(condition);
+        table.remove(dbObjectCondition);
+    }
+
     public void addNewObject(){
-        
+
     }
     
     public void createNewAggregationObject(){
+
+    }
+    
+    public void updateEventToObjectMapInMemoryDB(String object){
         
     }
-    
-    public static void main(String args[]){
-        String input = "{\"id\":\"eventId\",\"type\":\"eventType11\",\"test_cases\" : [{\"event_id\" : \"testcaseid1\", \"test_data\" : \"testcase1data\"},{\"event_id\" : \"testcaseid2\", \"test_data\" : \"testcase2data\"}]}";
-        updateEventToObjectMapInMemoryDB(input);
-    }
-    
-    public static void updateEventToObjectMapInMemoryDB(String object){
-        try {
-            MongoClient mongo = new MongoClient("localhost", 27017);
-            DB db = mongo.getDB("EventStorageDB");
-            DBCollection table = db.getCollection("SampleEvents");
-            DBObject dbObject = (DBObject)JSON.parse(object);
-            table.insert(dbObject);
-        }
-        catch (Exception e) {
-            e.getMessage();
-        }
-    }
-    
+
     public void saveAggreagatedObjectToInMemoryDB(){
-        
+
     }
 
 }
