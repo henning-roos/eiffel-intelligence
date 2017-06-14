7d4146cd86bb5678b45a958bc9a3050135d8e9d6
Suryakumari <sk.surya@tcs.com>
2017-06-14 20:29:17 +0530

    Implemented MongoDb querying part

--- src/main/java/com/ericsson/ei/mergeHandler/MergeHandler.java
+++ src/main/java/com/ericsson/ei/mergeHandler/MergeHandler.java
@@ -1,41 +1,21 @@
 package com.ericsson.ei.mergeHandler;
 
-import com.mongodb.DB;
-import com.mongodb.DBCollection;
-import com.mongodb.DBObject;
-import com.mongodb.MongoClient;
-import com.mongodb.util.JSON;
-
 public class MergeHandler {
-    
+
     public void addNewObject(){
-        
+
     }
     
     public void createNewAggregationObject(){
+
+    }
+    
+    public void updateEventToObjectMapInMemoryDB(String object){
         
     }
-    
-    public static void main(String args[]){
-        String input = "{\"id\":\"eventId\",\"type\":\"eventType11\",\"test_cases\" : [{\"event_id\" : \"testcaseid1\", \"test_data\" : \"testcase1data\"},{\"event_id\" : \"testcaseid2\", \"test_data\" : \"testcase2data\"}]}";
-        updateEventToObjectMapInMemoryDB(input);
-    }
-    
-    public static void updateEventToObjectMapInMemoryDB(String object){
-        try {
-            MongoClient mongo = new MongoClient("localhost", 27017);
-            DB db = mongo.getDB("EventStorageDB");
-            DBCollection table = db.getCollection("SampleEvents");
-            DBObject dbObject = (DBObject)JSON.parse(object);
-            table.insert(dbObject);
-        }
-        catch (Exception e) {
-            e.getMessage();
-        }
-    }
-    
+
     public void saveAggreagatedObjectToInMemoryDB(){
-        
+
     }
 
 }
---
+++ src/main/java/com/ericsson/ei/mongoDBHandler/MongoDBHandler.java
@@ -0,0 +1,126 @@
+package com.ericsson.ei.mongoDBHandler;
+
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+import com.mongodb.MongoClient;
+import com.mongodb.WriteResult;
+import com.mongodb.util.JSON;
+
+public class MongoDBHandler {
+    MongoClient mongoClient;
+
+    //Establishing the connection to mongodb and creating a collection
+    public  void createConnection(String host, int port){
+        mongoClient = new MongoClient(host , port);
+    }
+
+    //Insert data into collection
+    public  boolean insertDocument(String dataBaseName, String collectionName, String input){
+        try {
+            DB db = mongoClient.getDB(dataBaseName);
+            DBCollection table = db.getCollection(collectionName);
+            DBObject dbObjectInput = (DBObject) JSON.parse(input);
+            WriteResult result = table.insert(dbObjectInput);
+            if (result.wasAcknowledged()) {
+                System.out.println("Inserted successfully");
+                return result.wasAcknowledged();
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return false;
+    }
+
+    //Retrieve entire data from  collection
+    public boolean getAllDocuments(String dataBaseName, String collectionName){
+        try{
+            DB db = mongoClient.getDB(dataBaseName);
+            DBCollection table = db.getCollection(collectionName);
+            DBCursor cursor = table.find();
+            if (cursor.count()!=0){
+                int i = 1;
+                while (cursor.hasNext()) { 
+                    System.out.println("Inserted Document: "+i); 
+                    System.out.println(cursor.next()); 
+                    i++;
+                }
+                return true;
+            }
+            else{
+                System.out.println("No documents found");
+                return false;
+            }
+        }catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return false;
+    }
+
+    //Retrieve data from the collection based on condition
+    public  boolean getDocumentOnCondition(String dataBaseName, String collectionName,String condition){
+        try{
+            DB db = mongoClient.getDB(dataBaseName);
+            DBCollection table = db.getCollection(collectionName);
+            DBObject dbObjectCondition = (DBObject)JSON.parse(condition);
+            DBCursor conditionalCursor = table.find(dbObjectCondition);
+            if (conditionalCursor.count()!=0){
+                while(conditionalCursor.hasNext()) {
+                    System.out.println(conditionalCursor.next());
+                }
+                return true;
+            }
+            else{
+                System.out.println("No documents found with given condition");
+                return false;
+            }
+        }catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return false;
+    }
+
+    //update the document in collection
+    public  boolean updateDocument(String dataBaseName, String collectionName, String input, String updateInput ){
+        try{
+            boolean existing = getDocumentOnCondition(dataBaseName,collectionName,input);
+            if(existing){
+                DB db = mongoClient.getDB(dataBaseName);
+                DBCollection table = db.getCollection(collectionName);
+                DBObject dbObjectInput = (DBObject)JSON.parse(input);
+                DBObject dbObjectUpdateInput = (DBObject)JSON.parse(updateInput);
+                WriteResult result = table.update(dbObjectInput , dbObjectUpdateInput);
+                return result.isUpdateOfExisting();
+            }
+            else{
+                System.out.println("Provide valid input document that needs to get updated");
+                return false;
+            }
+        }catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return false;
+    }
+
+    //drop the document in collection
+    public  boolean dropDocument(String dataBaseName, String collectionName,String condition){
+        try{
+            DB db = mongoClient.getDB(dataBaseName);
+            DBCollection table = db.getCollection(collectionName);
+            DBObject dbObjectCondition = (DBObject)JSON.parse(condition);
+            WriteResult result = table.remove(dbObjectCondition);
+            if(result.getN()>0){
+                return true;
+            }
+            else{
+                System.out.println("No documents found to delete");
+                return false;
+            }
+        }catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return false;
+    }
+
+}
